5bca2a15c0e670095604d60ce5c930cb59d50e55
Filippo Garota <filogarota101@gmail.com>
2021-04-08 16:07:37 +0200

    Stampa degli album

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    albums: []
+  },
+  mounted: function(){
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+    .then((risposta) => {
+      this.albums = risposta.data.response;
+    });
+  }
+
+});
