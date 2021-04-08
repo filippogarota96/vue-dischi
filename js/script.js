51f6858fd2489364072fa362a9a9dcddce3b0e59
Filippo Garota <filogarota101@gmail.com>
2021-04-08 18:37:19 +0200

    select

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 var app = new Vue ({
   el: '#app',
   data: {
-    albums: []
+    albums: [],
+    genres: []
   },
   mounted: function(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
     .then((risposta) => {
       this.albums = risposta.data.response;
+      this.albums.forEach((element) => {
+        if (this.genres.includes(element) == false) {
+          this.genres.push(element);
+          console.log(this.genres);
+        }
+      });
     });
-  }
-
+  },
 });
