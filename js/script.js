992104004ee6fb336bbb981307d5110c0eb75bbd
Filippo Garota <filogarota101@gmail.com>
2021-04-09 10:00:11 +0200

    Selcet dei generi

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ var app = new Vue ({
   el: '#app',
   data: {
     albums: [],
-    genres: []
+    genres: [],
+    genreSelected: ''
   },
   mounted: function(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
     .then((risposta) => {
       this.albums = risposta.data.response;
-      this.albums.forEach((element) => {
-        if (this.genres.includes(element) == false) {
-          this.genres.push(element);
-          console.log(this.genres);
+      this.albums.forEach((item, i) => {
+        if (this.genres.includes(item.genre) == false) {
+          this.genres.push(item.genre);
         }
       });
     });
